a2b94a11a4e89509d3bc26f83745906810b94281
Rodrigo Rey <rodrigorey2005@gmail.com>
2026-04-08 02:14:28 -0300

    [RED] test: RegisterClient_PasswordNoUppercase_ThrowsArgumentException

--- Tests/DarkKitchen.BusinessLogic.Test/ClientServiceTests.cs
+++ Tests/DarkKitchen.BusinessLogic.Test/ClientServiceTests.cs
@@ -56,6 +56,13 @@ public class ClientServiceTests
     }
 
     [TestMethod]
+    public void RegisterClient_PasswordNoUppercase_ThrowsArgumentException()
+    {
+        Assert.ThrowsException<ArgumentException>(
+            () => _clientService.RegisterClient("Juan", "Garcia", "juan@test.com", "099123456", "nouppercase@1abc"));
+    }
+
+    [TestMethod]
     public void RegisterClient_ValidData_CallsRepositoryAdd()
     {
         _clientService.RegisterClient("Juan", "Garcia", "juan@test.com", "099123456", "ValidPass@1Ab!xyz");
