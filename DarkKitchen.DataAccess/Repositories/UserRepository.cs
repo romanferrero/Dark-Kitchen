304a38522d110a6e97b9565e0178511d4ca8dee2
Rodrigo Rey <rodrigorey2005@gmail.com>
2026-04-08 02:04:34 -0300

    [GREEN] feat: implementar UserRepository.Add

--- DarkKitchen.DataAccess/Repositories/UserRepository.cs
+++ DarkKitchen.DataAccess/Repositories/UserRepository.cs
@@ -5,6 +5,12 @@ namespace DarkKitchen.DataAccess.Repositories;
 
 public class UserRepository(AppDbContext context) : IUserRepository
 {
+    public void Add(User user)
+    {
+        context.Users.Add(user);
+        context.SaveChanges();
+    }
+
     public User? GetByEmail(string email)
     {
         return context.Users.FirstOrDefault(u => u.Email == email);
