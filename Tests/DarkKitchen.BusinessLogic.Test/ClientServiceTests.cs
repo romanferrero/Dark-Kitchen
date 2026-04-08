8388e88c3a8ac05ec4dadd567c9e12b5d465ad9d
Rodrigo Rey <rodrigorey2005@gmail.com>
2026-04-08 02:06:39 -0300

    [RED] test: RegisterClient_EmptyNombre_ThrowsArgumentException

--- Tests/DarkKitchen.BusinessLogic.Test/ClientServiceTests.cs
+++ Tests/DarkKitchen.BusinessLogic.Test/ClientServiceTests.cs
@@ -19,6 +19,13 @@ public class ClientServiceTests
     }
 
     [TestMethod]
+    public void RegisterClient_EmptyNombre_ThrowsArgumentException()
+    {
+        Assert.ThrowsException<ArgumentException>(
+            () => _clientService.RegisterClient(string.Empty, "Garcia", "juan@test.com", "099123456", "ValidPass@1Ab!xyz"));
+    }
+
+    [TestMethod]
     public void RegisterClient_ValidData_CallsRepositoryAdd()
     {
         _clientService.RegisterClient("Juan", "Garcia", "juan@test.com", "099123456", "ValidPass@1Ab!xyz");
