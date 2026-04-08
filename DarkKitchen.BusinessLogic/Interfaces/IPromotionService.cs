b72a28506ebc9937ccd727aabe1e97d1c4bc16d6
Santiago Pedetti <santiago.pedetti@gmail.com>
2026-04-08 17:31:32 -0300

    [RED] test: GetPromotions_ValidFilters_Returns200WithList

--- DarkKitchen.BusinessLogic/Interfaces/IPromotionService.cs
+++ DarkKitchen.BusinessLogic/Interfaces/IPromotionService.cs
@@ -3,4 +3,6 @@ namespace DarkKitchen.BusinessLogic.Interfaces;
 public interface IPromotionService
 {
     string CreatePromotion(string name, int discount, DateOnly dateFrom, DateOnly dateTo);
+
+    IEnumerable<string> GetPromotions(string? date, string? line, string? product);
 }
