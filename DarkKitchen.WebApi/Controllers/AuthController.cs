df5c539207edc9d1fa266ced50b1d065fa40fe91
Rodrigo Rey <rodrigorey2005@gmail.com>
2026-04-07 23:56:14 -0300

    [GREEN] feat: AuthController.Login retorna 200 con token

---
+++ DarkKitchen.WebApi/Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+using DarkKitchen.BusinessLogic.Interfaces;
+using DarkKitchen.WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DarkKitchen.WebApi.Controllers;
+
+[ApiController]
+[Route("api/auth")]
+public class AuthController : ControllerBase
+{
+    private readonly IAuthService _authService;
+
+    public AuthController(IAuthService authService)
+    {
+        _authService = authService;
+    }
+
+    [HttpPost("login")]
+    public IActionResult Login(LoginRequestModel request)
+    {
+        var token = _authService.Login(request.Email, request.Password);
+        return Ok(token);
+    }
+}
