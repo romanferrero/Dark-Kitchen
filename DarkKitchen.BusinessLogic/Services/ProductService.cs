49d6fc921b8096d95d232992c7220bc1e6b5579b
Roman <romaferrero@hotmail.com>
2026-04-09 04:09:04 -0300

    [GREEN] delegate line filter query from service to repository

--- DarkKitchen.BusinessLogic/Services/ProductService.cs
+++ DarkKitchen.BusinessLogic/Services/ProductService.cs
@@ -1,10 +1,23 @@
+using DarkKitchen.BusinessLogic.Interfaces;
 using DarkKitchen.Domain;
 
 namespace DarkKitchen.BusinessLogic.Services;
 
-public class ProductService
+public class ProductService(IProductRepository productRepository) : IProductService
 {
     public List<Product> GetProducts(string? line, List<string>? categories, string? name)
+    {
+        return productRepository.GetFiltered(line, categories, name);
+    }
+
+    public string CreateProduct(int code, string name, string description,
+                                string line, string category, string images, bool active)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string UpdateProduct(int code, string name, string description,
+                                string line, string category, string images, bool active)
     {
         throw new NotImplementedException();
     }
