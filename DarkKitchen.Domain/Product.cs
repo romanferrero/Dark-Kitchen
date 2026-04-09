07e5cf4cac97547df95adec89a801cc3c54a3c6b
Roman <romaferrero@hotmail.com>
2026-04-09 03:29:25 -0300

    add Product domain entity

---
+++ DarkKitchen.Domain/Product.cs
@@ -0,0 +1,22 @@
+namespace DarkKitchen.Domain;
+
+public class Product
+{
+    public int Id { get; set; }
+
+    public string Code { get; set; } = string.Empty;
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public string Line { get; set; } = string.Empty;
+
+    public string Category { get; set; } = string.Empty;
+
+    public decimal Price { get; set; }
+
+    public List<string> ImageUrls { get; set; } = [];
+
+    public bool Active { get; set; } = true;
+}
