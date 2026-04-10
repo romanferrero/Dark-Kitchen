df6bd0b5eb7dd217a7cd7ce57546e7f92adff240
Rodrigo Rey <rodrigorey2005@gmail.com>
2026-04-09 22:18:19 -0300

    [RED] test: CreateProduct_CodeTooShort_ThrowsArgumentException

--- Tests/DarkKitchen.BusinessLogic.Test/ProductServiceTests.cs
+++ Tests/DarkKitchen.BusinessLogic.Test/ProductServiceTests.cs
@@ -165,6 +165,20 @@ public class ProductServiceTests
     }
 
     [TestMethod]
+    public void CreateProduct_CodeTooShort_ThrowsArgumentException()
+    {
+        Assert.ThrowsException<ArgumentException>(() =>
+            _productService.CreateProduct(
+                "AB",
+                "Hamburguesa clasica",
+                "Hamburguesa con lechuga y tomate fresco",
+                "Combo burgers",
+                "Parrilla",
+                "http://img.com/burg1.jpg",
+                true));
+    }
+
+    [TestMethod]
     public void CreateProduct_ValidData_CallsRepositoryAdd()
     {
         _productRepoMock
