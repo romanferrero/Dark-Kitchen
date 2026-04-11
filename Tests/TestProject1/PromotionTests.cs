8052c56fe1f760eaaba40dc2ed664c9acfa447b2
Rodrigo Rey <rodrigorey2005@gmail.com>
2026-04-11 12:26:00 -0300

    [RED] test: AddProduct with valid product adds it to promotion list

--- Tests/TestProject1/PromotionTests.cs
+++ Tests/TestProject1/PromotionTests.cs
@@ -37,4 +37,16 @@ public class PromotionTests
         Assert.ThrowsException<ArgumentException>(() =>
             Promotion.Create("Black Friday", 10, new DateOnly(2026, 5, 31), new DateOnly(2026, 5, 1)));
     }
+
+    [TestMethod]
+    public void AddProduct_ValidProduct_AddsToList()
+    {
+        var promotion = Promotion.Create("Black Friday", 10, new DateOnly(2026, 5, 1), new DateOnly(2026, 5, 31));
+        var product = Product.Create("BURG01", "Hamburguesa clasica", "Hamburguesa con lechuga y tomate fresco", "Combo burgers", "Parrilla", "http://img.com/b.jpg", true);
+
+        promotion.AddProduct(product);
+
+        Assert.AreEqual(1, promotion.Products.Count);
+        Assert.AreEqual("BURG01", promotion.Products[0].Code);
+    }
 }
