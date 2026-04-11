64e32e041cad8d3aa8b3636a0caf71cd8a770a86
Rodrigo Rey <rodrigorey2005@gmail.com>
2026-04-11 12:26:55 -0300

    [RED] test: AddProduct with duplicate product throws InvalidOperationException

--- Tests/TestProject1/PromotionTests.cs
+++ Tests/TestProject1/PromotionTests.cs
@@ -49,4 +49,14 @@ public class PromotionTests
         Assert.AreEqual(1, promotion.Products.Count);
         Assert.AreEqual("BURG01", promotion.Products[0].Code);
     }
+
+    [TestMethod]
+    public void AddProduct_DuplicateProduct_ThrowsInvalidOperationException()
+    {
+        var promotion = Promotion.Create("Black Friday", 10, new DateOnly(2026, 5, 1), new DateOnly(2026, 5, 31));
+        var product = Product.Create("BURG01", "Hamburguesa clasica", "Hamburguesa con lechuga y tomate fresco", "Combo burgers", "Parrilla", "http://img.com/b.jpg", true);
+        promotion.AddProduct(product);
+
+        Assert.ThrowsException<InvalidOperationException>(() => promotion.AddProduct(product));
+    }
 }
