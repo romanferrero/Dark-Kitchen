6eb0bdc6d863e5b1a263759ef02631d118dd201b
Roman <romaferrero@hotmail.com>
2026-04-11 22:30:31 -0300

    [RED] Add test CreateOrder_ProductNotFound_ThrowsKeyNotFoundException

--- Tests/DarkKitchen.BusinessLogic.Test/OrderServiceTests.cs
+++ Tests/DarkKitchen.BusinessLogic.Test/OrderServiceTests.cs
@@ -127,4 +127,26 @@ public class OrderServiceTests
 
         Assert.AreEqual(800m, result.Subtotal);
     }
+
+    [TestMethod]
+    public void CreateOrder_ProductNotFound_ThrowsKeyNotFoundException()
+    {
+        _productRepoMock
+            .Setup(r => r.GetByCode("NOEXIST"))
+            .Returns((Product?)null);
+
+        var items = new List<(string ProductCode, int Quantity)>
+        {
+            ("NOEXIST", 1),
+        };
+
+        Assert.ThrowsException<KeyNotFoundException>(() =>
+            _orderService.CreateOrder(
+                clientId: 1,
+                deliveryType: "express",
+                street: "18 de Julio",
+                doorNumber: "1234",
+                apartment: "Apto 101",
+                items: items));
+    }
 }
