b66e581bd3e8a594cfc48e219bc031b54e4f5d4e
Roman <romaferrero@hotmail.com>
2026-04-12 00:32:14 -0300

    [GREEN] satisfy address persistence test

--- Tests/DarkKitchen.DataAccess.Test/OrderRepositoryTests.cs
+++ Tests/DarkKitchen.DataAccess.Test/OrderRepositoryTests.cs
@@ -93,4 +93,62 @@ public class OrderRepositoryTests
         Assert.AreEqual(user.Id, saved.ClientId);
         Assert.AreEqual(1, saved.Items.Count);
     }
+
+    [TestMethod]
+    public void Add_ValidOrder_PersistsAddress()
+    {
+        var product = Product.Create(
+            code: "BURG01",
+            name: "Hamburguesa clasica",
+            description: "Hamburguesa con lechuga y tomate fresco",
+            line: "Combo burgers",
+            category: "Parrilla",
+            images: "http://img.com/burg1.jpg|100",
+            active: true);
+
+        var user = new User
+        {
+            FirstName = "Roman",
+            LastName = "Ferrero",
+            Email = "roman@test.com",
+            Phone = "099123456",
+            Password = "Password15365!!",
+            Role = UserRole.Client
+        };
+        _context.Users.Add(user);
+        _context.SaveChanges();
+
+        var address = new Address
+        {
+            Street = "18 de Julio",
+            DoorNumber = "1234",
+            Apartment = "Apto 101",
+        };
+
+        var items = new List<OrderItem>
+    {
+        new OrderItem
+        {
+            ProductId = product.Id,
+            Product = product,
+            Quantity = 2,
+            UnitPrice = 200m,
+        },
+    };
+
+        var order = Order.Create(
+            clientId: user.Id,
+            deliveryType: DeliveryType.Express,
+            address: address,
+            items: items);
+
+        _repository.Add(order);
+
+        var saved = _context.Orders.FirstOrDefault();
+
+        Assert.IsNotNull(saved);
+        Assert.AreEqual("18 de Julio", saved.Address.Street);
+        Assert.AreEqual("1234", saved.Address.DoorNumber);
+        Assert.AreEqual("Apto 101", saved.Address.Apartment);
+    }
 }
