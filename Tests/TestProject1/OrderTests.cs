eb76b8fcd79888873f0dd3ea08560714359a8b1c
Santiago Pedetti <santiago.pedetti@gmail.com>
2026-04-14 16:26:23 -0300

    [RED]test: CreateOrder_Invalid_OrderNumber

--- Tests/TestProject1/OrderTests.cs
+++ Tests/TestProject1/OrderTests.cs
@@ -104,4 +104,20 @@ public class OrderTests
             2,
             12);
     }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentOutOfRangeException))]
+    public void CreateOrder_Invalid_OrderNumber()
+    {
+        Order.Create(
+            _orderId,
+            _deliveryType,
+            _address,
+            new List<Product> { _product },
+            100,
+            -1,
+            10,
+            2,
+            12);
+    }
 }
