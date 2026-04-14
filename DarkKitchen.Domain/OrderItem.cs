2bbb657398756e026ab24591f45720b901a93cc3
Santiago Pedetti <santiago.pedetti@gmail.com>
2026-04-14 07:22:57 -0300

    [GREEN] test: Create_ShouldThrowException_WhenQuantityIsZero

--- DarkKitchen.Domain/OrderItem.cs
+++ DarkKitchen.Domain/OrderItem.cs
@@ -2,18 +2,85 @@ namespace DarkKitchen.Domain;
 
 public class OrderItem
 {
-    private OrderItem() { }
+    private int _id;
+    private int _productId;
+    private Product _product = null!;
+    private int _quantity;
+    private decimal _originalPrice;
+    private decimal _unitPrice;
+    private string? _promotionName;
+    private int? _discountPercentage;
+    private int _orderId;
+    private Order _order = null!;
 
-    public int Id { get; set; }
-    public int ProductId { get; private set; }
-    public Product Product { get; private set; } = null!;
-    public int Quantity { get; private set; }
-    public decimal OriginalPrice { get; private set; }
-    public decimal UnitPrice { get; private set; }
-    public string? PromotionName { get; private set; }
-    public int? DiscountPercentage { get; private set; }
-    public int OrderId { get; set; }
-    public Order Order { get; set; } = null!;
+    private OrderItem() { }
+    public int Id
+    {
+        get => _id;
+        set => _id = value;
+    }
+
+    public int ProductId
+    {
+        get => _productId;
+        private set => _productId = value;
+    }
+
+    public Product Product
+    {
+        get => _product;
+        private set => _product = value;
+    }
+
+    public int Quantity
+    {
+        get => _quantity;
+        private set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Quantity must be positive.", nameof(value));
+            }
+
+            _quantity = value;
+        }
+    }
+
+    public decimal OriginalPrice
+    {
+        get => _originalPrice;
+        private set => _originalPrice = value;
+    }
+
+    public decimal UnitPrice
+    {
+        get => _unitPrice;
+        private set => _unitPrice = value;
+    }
+
+    public string? PromotionName
+    {
+        get => _promotionName;
+        private set => _promotionName = value;
+    }
+
+    public int? DiscountPercentage
+    {
+        get => _discountPercentage;
+        private set => _discountPercentage = value;
+    }
+
+    public int OrderId
+    {
+        get => _orderId;
+        set => _orderId = value;
+    }
+
+    public Order Order
+    {
+        get => _order;
+        set => _order = value;
+    }
 
     public static OrderItem Create(
         Product product,
@@ -22,7 +89,7 @@ public class OrderItem
         decimal unitPrice,
         string? promotionName,
         int? discountPercentage)
-        {
+    {
         return new OrderItem
         {
             ProductId = product.Id,
