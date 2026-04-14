9aa25924be132e050b3f9469d49843c7d6645fa4
Rodrigo Rey <rodrigorey2005@gmail.com>
2026-04-13 21:25:52 -0300

    [red]: OnException_ArgumentException should return 400

---
+++ Tests/DarkKitchen.WebApi.Test/FiltersTests/CustomExceptionFilterTests.cs
@@ -0,0 +1,41 @@
+using DarkKitchen.WebApi.Filters;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
+
+namespace DarkKitchen.WebApi.Test.FiltersTests;
+
+[TestClass]
+public class CustomExceptionFilterTests
+{
+    private CustomExceptionFilter _filter = null!;
+
+    [TestInitialize]
+    public void Initialize()
+    {
+        _filter = new CustomExceptionFilter();
+    }
+
+    private static ExceptionContext BuildContext(Exception exception)
+    {
+        var actionContext = new ActionContext(
+            new DefaultHttpContext(),
+            new RouteData(),
+            new ActionDescriptor());
+        return new ExceptionContext(actionContext, []) { Exception = exception };
+    }
+
+    [TestMethod]
+    public void OnException_ArgumentException_Returns400()
+    {
+        var context = BuildContext(new ArgumentException("bad input"));
+
+        _filter.OnException(context);
+
+        var result = context.Result as ObjectResult;
+        Assert.IsNotNull(result);
+        Assert.AreEqual(400, result.StatusCode);
+    }
+}
