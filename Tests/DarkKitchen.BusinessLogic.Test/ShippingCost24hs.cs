26f354c716ef4179e0578652fe4c7c5a105468ac
Santiago Pedetti <santiago.pedetti@gmail.com>
2026-04-15 15:33:37 -0300

    fixed variable name os shipping cost in tests bc of actions

--- Tests/DarkKitchen.BusinessLogic.Test/ShippingCost24hs.cs
+++ Tests/DarkKitchen.BusinessLogic.Test/ShippingCost24hs.cs
@@ -5,18 +5,18 @@ namespace DarkKitchen.BusinessLogic.Test;
 [TestClass]
 public class ShippingCost24hsCalculatorTests
 {
-    private ShippingCost24hsCalculator _24hsCalculator111 = null!;
+    private ShippingCost24hsCalculator _shippingCost24HsCalculator = null!;
 
     [TestInitialize]
     public void Initialize()
     {
-        _24hsCalculator111 = new ShippingCost24hsCalculator();
+        _shippingCost24HsCalculator = new ShippingCost24hsCalculator();
     }
 
     [TestMethod]
     public void Calculate_24hs_Returns10()
     {
-        var cost = _24hsCalculator111.GetCost();
+        var cost = _shippingCost24HsCalculator.GetCost();
 
         Assert.AreEqual(10, cost);
     }
