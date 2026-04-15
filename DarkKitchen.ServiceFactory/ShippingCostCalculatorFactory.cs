0ed5a5a6aad391882e0ad205dafaac18a7b9ae73
Santiago Pedetti <santiago.pedetti@gmail.com>
2026-04-15 18:02:32 -0300

    fixed warnings at calculatorFactory

--- DarkKitchen.ServiceFactory/ShippingCostCalculatorFactory.cs
+++ DarkKitchen.ServiceFactory/ShippingCostCalculatorFactory.cs
@@ -6,17 +6,13 @@ namespace DarkKitchen.ServiceFactory;
 
 public class ShippingCostCalculatorFactory(IEnumerable<IShippingCostCalculator> calculators) : IShippingCostCalculatorFactory
 {
-    private readonly IEnumerable<IShippingCostCalculator> _calculators = calculators;
-
     public IShippingCostCalculator GetCalculator(DeliveryType deliveryType)
     {
         return deliveryType switch
         {
-            DeliveryType.Express => _calculators
-                .First(c => c is ShippingCostExpressCalculator),
+            DeliveryType.Express => calculators.First(c => c is ShippingCostExpressCalculator),
 
-            DeliveryType.TwentyFourHours => _calculators
-                .First(c => c is ShippingCost24hsCalculator),
+            DeliveryType.TwentyFourHours => calculators.First(c => c is ShippingCost24hsCalculator),
 
             _ => throw new ArgumentException("Invalid delivery type")
         };
