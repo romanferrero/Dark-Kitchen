d93d0d7c07dfec60e80aaf20efa9784fabe16e25
Roman <romaferrero@hotmail.com>
2026-04-16 19:02:25 -0300

    [GREEN] implement User.Update

--- DarkKitchen.Domain/User.cs
+++ DarkKitchen.Domain/User.cs
@@ -39,6 +39,15 @@ public class User
         };
     }
 
+    public void Update(string firstName, string lastName, string email, string phone, string password)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        Phone = phone;
+        Password = password;
+    }
+
     public int Id { get; set; }
 
     public string FirstName
