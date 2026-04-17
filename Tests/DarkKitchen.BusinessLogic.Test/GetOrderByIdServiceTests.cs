c2e6424d1b1bffcaa1a2fe2639537d659a49c527
Rodrigo Rey <rodrigorey2005@gmail.com>
2026-04-16 21:08:32 -0300

    test(red): tie promotions should use alphabetical name

--- Tests/DarkKitchen.BusinessLogic.Test/GetOrderByIdServiceTests.cs
+++ Tests/DarkKitchen.BusinessLogic.Test/GetOrderByIdServiceTests.cs
@@ -144,4 +144,65 @@ public class GetOrderByIdServiceTests
 
         Assert.AreEqual("Unknown client", result.ClientFullName);
     }
+
+    [TestMethod]
+    public void GetOrderById_WhenPromotionsHaveSameDiscount_UsesAlphabeticalNameAsTieBreaker()
+    {
+        var clientId = 1;
+
+        var user = new User
+        {
+            Id = clientId,
+            FirstName = "Juan",
+            LastName = "Garcia",
+            Email = "juan@test.com",
+            Phone = "099123456",
+            Password = "ValidPass1!extra",
+            Role = UserRole.Client
+        };
+
+        var product = Product.Create(
+            "PROD-001",
+            "Producto de prueba uno",
+            "Descripcion valida del producto numero uno",
+            "LineA",
+            "CategoryA",
+            "img.jpg|10",
+            true);
+        product.Price = 100m;
+
+        var address = Address.Create("18 de Julio", "1234", "3B");
+
+        var order = Order.Create(
+            orderId: 0,
+            deliveryType: DeliveryType.Express,
+            address: address,
+            products: [product],
+            clientId: clientId,
+            orderNumber: 101,
+            subtotal: 100.0,
+            shippingCost: 20.0,
+            totalCost: 146.4);
+
+        var promoB = Promotion.Create("B Promo", 20, DateOnly.FromDateTime(DateTime.Today), DateOnly.FromDateTime(DateTime.Today));
+        promoB.AddProduct(product);
+
+        var promoA = Promotion.Create("A Promo", 20, DateOnly.FromDateTime(DateTime.Today), DateOnly.FromDateTime(DateTime.Today));
+        promoA.AddProduct(product);
+
+        _orderRepoMock.Setup(r => r.GetOrderById(101)).Returns(order);
+
+        _userRepoMock
+            .Setup(r => r.GetAll(It.IsAny<Expression<Func<User, bool>>>() ))
+            .Returns([user]);
+
+        _promotionRepoMock
+            .Setup(r => r.GetAll(It.IsAny<Expression<Func<Promotion, bool>>>() ))
+            .Returns([promoB, promoA]);
+
+        var result = _orderService.GetOrderById(101);
+
+        Assert.AreEqual("A Promo", result.Products[0].PromotionName);
+        Assert.AreEqual(20, result.Products[0].DiscountPercentage);
+    }
 }
