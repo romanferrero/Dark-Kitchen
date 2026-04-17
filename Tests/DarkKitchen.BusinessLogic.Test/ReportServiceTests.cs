89f5b1c92a5a24b3a4d55e4cc78cc5ca750beaa5
Roman <romaferrero@hotmail.com>
2026-04-17 02:10:02 -0300

    [REFACTOR] refactor tests

--- Tests/DarkKitchen.BusinessLogic.Test/ReportServiceTests.cs
+++ Tests/DarkKitchen.BusinessLogic.Test/ReportServiceTests.cs
@@ -8,6 +8,9 @@ namespace DarkKitchen.BusinessLogic.Test;
 [TestClass]
 public class ReportServiceTests
 {
+    private static readonly DateTime DateFrom = new(2026, 1, 1);
+    private static readonly DateTime DateTo = new(2026, 12, 31);
+
     private Mock<IRepository<Order>> _orderRepoMock = null!;
     private Mock<IRepository<User>> _userRepoMock = null!;
     private ReportService _reportService = null!;
@@ -15,8 +18,8 @@ public class ReportServiceTests
     [TestInitialize]
     public void Setup()
     {
-        _orderRepoMock = new Mock<IRepository<Order>>();
-        _userRepoMock = new Mock<IRepository<User>>();
+        _orderRepoMock = new Mock<IRepository<Order>>(MockBehavior.Strict);
+        _userRepoMock = new Mock<IRepository<User>>(MockBehavior.Strict);
         _reportService = new ReportService(_orderRepoMock.Object, _userRepoMock.Object);
     }
 
@@ -27,34 +30,29 @@ public class ReportServiceTests
             .Setup(r => r.GetAll(null))
             .Returns(new List<Order>());
 
-        var result = _reportService.GetTopProducts(
-            new DateTime(2026, 1, 1),
-            new DateTime(2026, 12, 31));
+        var result = _reportService.GetTopProducts(DateFrom, DateTo);
 
+        Assert.IsNotNull(result);
         Assert.AreEqual(0, result.Count);
     }
 
     [TestMethod]
     public void GetTopProducts_WithOrders_ReturnsProductsOrderedByQuantity()
     {
-        var productA = Product.Create("PROD01", "Producto AAA uno", "Descripcion larga del producto A", "Linea1",
-            "Cat1", "http://img.com/a.jpg", true);
-        var productB = Product.Create("PROD02", "Producto BBB dos", "Descripcion larga del producto B", "Linea1",
-            "Cat1", "http://img.com/b.jpg", true);
+        var productA = CreateProduct("PROD01", "Producto AAA uno");
+        var productB = CreateProduct("PROD02", "Producto BBB dos");
 
-        var address = Address.Create("Calle", "123", "Apt");
-
-        var order1 = Order.Create(1, DeliveryType.Express, address, new List<Product> { productA, productB }, 1, 1, 100,
-            10, 110);
-        var order2 = Order.Create(2, DeliveryType.Express, address, new List<Product> { productA }, 1, 2, 50, 10, 60);
+        var orders = new List<Order>
+        {
+            CreateOrder(1, new List<Product> { productA, productB }),
+            CreateOrder(2, new List<Product> { productA }),
+        };
 
         _orderRepoMock
             .Setup(r => r.GetAll(null))
-            .Returns(new List<Order> { order1, order2 });
+            .Returns(orders);
 
-        var result = _reportService.GetTopProducts(
-            DateTime.Now.AddDays(-1),
-            DateTime.Now.AddDays(1));
+        var result = _reportService.GetTopProducts(DateFrom, DateTo);
 
         Assert.AreEqual(2, result.Count);
         Assert.AreEqual("PROD01", result[0].Code);
@@ -66,30 +64,63 @@ public class ReportServiceTests
     [TestMethod]
     public void GetTopProducts_MoreThanFiveProducts_ReturnsOnlyTopFive()
     {
-        var address = Address.Create("Calle", "123", "Apt");
-        var products = new List<Product>();
+        var products = Enumerable.Range(1, 7)
+            .Select(i => CreateProduct($"PROD0{i}", $"Producto numero {i:D2}"))
+            .ToList();
 
-        for(var i = 1; i <= 7; i++)
-        {
-            products.Add(Product.Create($"PROD0{i}", $"Producto numero {i:D2}", $"Descripcion larga producto {i:D2}",
-                "Linea1", "Cat1", $"http://img.com/{i}.jpg", true));
-        }
-
-        var orders = new List<Order>();
-        for(var i = 0; i < 7; i++)
-        {
-            var orderProducts = Enumerable.Repeat(products[i], i + 1).ToList();
-            orders.Add(Order.Create(i, DeliveryType.Express, address, orderProducts, 1, i, 100, 10, 110));
-        }
+        var orders = Enumerable.Range(0, 7)
+            .Select(i => CreateOrder(i, Enumerable.Repeat(products[i], i + 1).ToList()))
+            .ToList();
 
         _orderRepoMock
             .Setup(r => r.GetAll(null))
             .Returns(orders);
 
-        var result = _reportService.GetTopProducts(
-            DateTime.Now.AddDays(-1),
-            DateTime.Now.AddDays(1));
+        var result = _reportService.GetTopProducts(DateFrom, DateTo);
 
         Assert.AreEqual(5, result.Count);
+    }
+
+    private static Product CreateProduct(string code, string name)
+    {
+        return Product.Create(
+            code,
+            name,
+            $"Descripcion larga de {name}",
+            "Linea1",
+            "Cat1",
+            $"http://img.com/{code}.jpg",
+            true);
+    }
+
+    private static Order CreateOrder(int id, List<Product> products)
+    {
+        return Order.Create(
+            id,
+            DeliveryType.Express,
+            Address.Create("Calle", "123", "Apt"),
+            products,
+            1,
+            id,
+            100,
+            10,
+            110);
+    }
+
+    [TestMethod]
+    public void GetSalesReport_NoOrders_ReturnsEmptyReport()
+    {
+        _orderRepoMock
+            .Setup(r => r.GetAll(null))
+            .Returns(new List<Order>());
+
+        _userRepoMock
+            .Setup(r => r.GetAll(null))
+            .Returns(new List<User>());
+
+        var result = _reportService.GetSalesReport();
+
+        Assert.AreEqual(0, result.MonthlySales.Count);
+        Assert.AreEqual(0, result.GrandTotal);
     }
 }
