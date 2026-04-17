d45f5f9d9ec4183b6136f6be35962485d7b355f9
Roman <romaferrero@hotmail.com>
2026-04-17 01:38:33 -0300

    [GREEN] add HttpGet route to GetTopProducts

--- DarkKitchen.WebApi/Controllers/AdminReportsController.cs
+++ DarkKitchen.WebApi/Controllers/AdminReportsController.cs
@@ -6,6 +6,7 @@ namespace DarkKitchen.WebApi.Controllers;
 [ApiController]
 public class AdminReportsController(IReportService reportService) : ControllerBase
 {
+    [HttpGet("top-products")]
     public IActionResult GetTopProducts(DateTime dateFrom, DateTime dateTo)
     {
         var result = reportService.GetTopProducts(dateFrom, dateTo);
