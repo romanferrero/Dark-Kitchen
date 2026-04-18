a97bbf4035c9a0fbb3dc7f21ed49c0a98c60ead5
Roman <romaferrero@hotmail.com>
2026-04-18 20:21:05 -0300

    [RED] GetTopSellingProducts returns products ordered by quantity sold

--- Tests/DarkKitchen.DataAccess.Test/OrderRepositoryTests.cs
+++ Tests/DarkKitchen.DataAccess.Test/OrderRepositoryTests.cs
@@ -42,4 +42,40 @@ public class OrderRepositoryTests
         Assert.IsNotNull(result);
         Assert.AreEqual(0, result.Count);
     }
+
+    [TestMethod]
+    public void GetTopSellingProducts_WithOrders_ReturnsProductsOrderedByQuantity()
+    {
+        var productA = Product.Create(
+            "PRODA", "Hamburguesa Clásica", "Descripcion del producto test",
+            "Minutas clásicas", "Fritos", "http://img.com/burger.jpg|100", true);
+        var productB = Product.Create(
+            "PRODB", "Pizza Muzzarella Grande", "Descripcion del producto test",
+            "Minutas clásicas", "Fritos", "http://img.com/pizza.jpg|100", true);
+
+        _context.Products.AddRange(productA, productB);
+        _context.SaveChanges();
+
+        var order1 = Order.Create(1, DeliveryType.Express,
+            Address.Create("Calle", "123", "Apto 1"),
+            new List<Product> { productA, productB }, 1, 1, 100.0, 50.0, 150.0);
+        order1.OrderDate = new DateTime(2026, 1, 10);
+
+        var order2 = Order.Create(2, DeliveryType.Express,
+            Address.Create("Calle", "456", "Apto 2"),
+            new List<Product> { productA }, 2, 2, 100.0, 50.0, 150.0);
+        order2.OrderDate = new DateTime(2026, 1, 15);
+
+        _context.Orders.AddRange(order1, order2);
+        _context.SaveChanges();
+
+        var result = _repository.GetTopSellingProducts(
+            new DateTime(2026, 1, 1), new DateTime(2026, 1, 31), 5);
+
+        Assert.AreEqual(2, result.Count);
+        Assert.AreEqual("PRODA", result[0].Code);
+        Assert.AreEqual(2, result[0].QuantitySold);
+        Assert.AreEqual("PRODB", result[1].Code);
+        Assert.AreEqual(1, result[1].QuantitySold);
+    }
 }
