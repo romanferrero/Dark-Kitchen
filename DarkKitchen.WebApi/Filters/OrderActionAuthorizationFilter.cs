3aa80488175acb8119fb8e3eaf29f96eed1471f7
Santiago Pedetti <santiago.pedetti@gmail.com>
2026-04-25 07:23:35 -0300

    [GREEN]: OnAuthorization_NoRole_Returns403

--- DarkKitchen.WebApi/Filters/OrderActionAuthorizationFilter.cs
+++ DarkKitchen.WebApi/Filters/OrderActionAuthorizationFilter.cs
@@ -7,4 +7,12 @@ namespace DarkKitchen.WebApi.Filters;
 [AttributeUsage(AttributeTargets.Method)]
 public sealed class OrderActionAuthorizationFilter : Attribute, IAuthorizationFilter
 {
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        var roleString = context.HttpContext.Items["UserRole"]?.ToString();
+        if (!Enum.TryParse<UserRole>(roleString, out _))
+        {
+            context.Result = new ObjectResult("Forbidden") { StatusCode = 403 };
+        }
+    }
 }
