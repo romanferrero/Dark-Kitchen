858827d70ed216c1d7991d5c727029664363d334
Santiago Pedetti <santiago.pedetti@gmail.com>
2026-04-27 11:36:50 -0300

    [RED]: Create_WhitespaceCategory_ThrowsArgumentException

--- Tests/TestProject1/EntitiesTests/ProductTests.cs
+++ Tests/TestProject1/EntitiesTests/ProductTests.cs
@@ -224,4 +224,34 @@ public class ProductTests
                 "http://img.com/burg1.jpg",
                 true));
     }
+
+    [TestMethod]
+    public void Create_EmptyCategory_ThrowsArgumentException()
+    {
+        Assert.ThrowsException<ArgumentException>(() =>
+            Product.Create(
+                "BURG01",
+                "Hamburguesa clasica",
+                100m,
+                "Hamburguesa con lechuga y tomate fresco",
+                "Combo burgers",
+                string.Empty,
+                "http://img.com/burg1.jpg",
+                true));
+    }
+
+    [TestMethod]
+    public void Create_WhitespaceCategory_ThrowsArgumentException()
+    {
+        Assert.ThrowsException<ArgumentException>(() =>
+            Product.Create(
+                "BURG01",
+                "Hamburguesa clasica",
+                100m,
+                "Hamburguesa con lechuga y tomate fresco",
+                "Combo burgers",
+                "   ",
+                "http://img.com/burg1.jpg",
+                true));
+    }
 }
