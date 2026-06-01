122d7cb6e00b738efa393f1b6d602f24cf9da0aa
Santiago Pedetti <santiago.pedetti@gmail.com>
2026-06-01 12:32:45 -0300

    [GREEN]: UpdateStatus_DelayedToCancelled_Succeeds

--- Tests/DarkKitchen.BusinessLogic.Test/EntitiesTests/OrderTests.cs
+++ Tests/DarkKitchen.BusinessLogic.Test/EntitiesTests/OrderTests.cs
@@ -171,4 +171,15 @@ public class OrderTests
 
         Assert.AreEqual("Prepared", order.State.Name);
     }
+
+    [TestMethod]
+    public void UpdateStatus_DelayedToCancelled_Succeeds()
+    {
+        var order = BuildOrder();
+        order.UpdateStatus("Delayed");
+
+        order.UpdateStatus("Cancelled");
+
+        Assert.AreEqual("Cancelled", order.State.Name);
+    }
 }
