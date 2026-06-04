bf181b8f0cf4460e7655726f3cf9f28e814a7947
Santiago Pedetti <santiago.pedetti@gmail.com>
2026-06-04 17:28:36 -0300

    [RED]: GetAuditLogs_DateFromEqualDateTo_ThrowsArgumentException

---
+++ Tests/DarkKitchen.BusinessLogic.Test/ServicesTests/AuditLogServiceTests.cs
@@ -0,0 +1,28 @@
+using DarkKitchen.BusinessLogic.Services;
+using DarkKitchen.IDataAccess.RepositoriesInterfaces;
+using Moq;
+
+namespace DarkKitchen.BusinessLogic.Test.ServicesTests;
+
+[TestClass]
+public class AuditLogServiceTests
+{
+    private Mock<IAuditLogRepository> _auditRepoMock = null!;
+    private AuditLogService _auditService = null!;
+
+    [TestInitialize]
+    public void Initialize()
+    {
+        _auditRepoMock = new Mock<IAuditLogRepository>(MockBehavior.Strict);
+        _auditService = new AuditLogService(_auditRepoMock.Object);
+    }
+
+    [TestMethod]
+    public void GetAuditLogs_DateFromEqualDateTo_ThrowsArgumentException()
+    {
+        var date = new DateTime(2026, 4, 23, 8, 0, 0);
+
+        Assert.ThrowsException<ArgumentException>(() =>
+            _auditService.GetAuditLogs(date, date));
+    }
+}
