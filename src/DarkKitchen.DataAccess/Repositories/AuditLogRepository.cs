1710150571cf7121990afac8efdfe2b2ac765104
Santiago Pedetti <santiago.pedetti@gmail.com>
2026-06-04 17:16:58 -0300

    [REFACTOR]: get filtered auditoryRepository

---
+++ src/DarkKitchen.DataAccess/Repositories/AuditLogRepository.cs
@@ -0,0 +1,17 @@
+using DarkKitchen.Domain.Entities;
+using DarkKitchen.IDataAccess.RepositoriesInterfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace DarkKitchen.DataAccess.Repositories;
+
+public class AuditLogRepository(DbContext context) : Repository<AuditLog>(context), IAuditLogRepository
+{
+    public List<AuditLog> GetFiltered(DateTime dateFrom, DateTime dateTo, string? entityName = null, int? entityId = null)
+    {
+        return Context.Set<AuditLog>()
+            .Where(a => a.Timestamp >= dateFrom && a.Timestamp <= dateTo)
+            .Where(a => entityName == null || a.EntityName == entityName)
+            .Where(a => entityId == null || a.EntityId == entityId)
+            .ToList();
+    }
+}
