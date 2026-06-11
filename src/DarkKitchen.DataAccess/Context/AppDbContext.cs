940a524c54b771c9d10cf9d1442acd651b1147f2
Roman <romaferrero@hotmail.com>
2026-06-10 21:40:56 -0300

    fix: avoid nvarchar(max) literal so sqlite tests pass

--- src/DarkKitchen.DataAccess/Context/AppDbContext.cs
+++ src/DarkKitchen.DataAccess/Context/AppDbContext.cs
@@ -121,8 +121,7 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
                 .ValueGeneratedOnAdd();
 
             entity.Property(pi => pi.Url)
-                .IsRequired()
-                .HasColumnType("nvarchar(max)");
+                .IsRequired();
 
             entity.Property(pi => pi.SizeInKb)
                 .HasColumnType("decimal(10,2)");
