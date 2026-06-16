8ca2adb1c0387a291a4f57379b3b4266e3887c24
Rodrigo Rey <rodrigorey2005@gmail.com>
2026-06-16 17:16:47 -0300

    chore: regenerar empty_schema.sql y seed_data.sql + squash de migraciones
    - Squash de las 7 migraciones en una unica Initial acorde al modelo + SeedData
    - empty_schema.sql: 8 tablas con tipos correctos, sin datos
    - seed_data.sql: Users/DeliveryTypes del HasData (hashes reales) + datos demo
      consistentes (ClientId validos, DeliveryType validos, totales recalculados)
    - Verificado ejecutando ambos scripts contra SQL Server

--- src/DarkKitchen.DataAccess/Migrations/20260616200607_Initial.Designer.cs
+++ src/DarkKitchen.DataAccess/Migrations/20260616200607_Initial.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DarkKitchen.DataAccess.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20260611045227_SeedAdditionalUsers")]
-    partial class SeedAdditionalUsers
+    [Migration("20260616200607_Initial")]
+    partial class Initial
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
